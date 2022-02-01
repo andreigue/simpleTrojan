3a4a0328f2629037389fab40ee7c327f930f3684
Refat Kamal <refat.k17@gmail.com>
2022-02-01 17:27:16 -0500

    #4 get file list from base folders

--- fileStruct.py
+++ fileStruct.py
@@ -1,8 +1,18 @@
 import os
-from turtle import down
 
 folders = ["Desktop", "Documents", "Downloads"]    
 
-folder_list = [os.path.join(os.environ['USERPROFILE'], x) for x in folders]
+# TODO: Consider system with multiple drives
+folderPathList = [os.path.join(os.environ['USERPROFILE'], x) for x in folders]
 
 # look for "bank" within file/folder names
+def getFilesInPath(path):
+    fileList = [f for f in os.listdir(path) if "bank" in f.lower()]
+    return fileList
+
+for path in folderPathList:
+    fileList = getFilesInPath(path)
+
+
+# test code
+# getFilesInPath(os.path.abspath(""))
