498cc919b5bf7fc749e4f079af0a5f4d1e4c2609
Chris <andrei.guevorkian@mail.mcgill.ca>
2022-02-07 18:55:48 -0500

    #5 server/client connection works locally (on a single computer)

--- client.py
+++ client.py
@@ -3,14 +3,15 @@ import os
 
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096  # send 4096 bytes each time step
-HOST_IP = "XX.XX.XX.XXX" # public IP address of server 
+HOST_IP = "127.0.0.1" # public IP address of server 
 PORT = 5001
 
-s = socket.socket()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def openConnection():
     print("Opening Connection")
     s.connect((HOST_IP, PORT))
+    print("Connection established")
 
 def closeConnection():
     print("Closing Connection")
@@ -24,13 +25,17 @@ def sendFile(fileAbsPath):
     with open(fileAbsPath, "rb") as f:
         totalRead = 0
         while True:
-            bytesRead = f.read(BUFFER_SIZE)
-            if not bytesRead:
+            dataRead = f.read(BUFFER_SIZE)
+            if not dataRead:
                 # file transmitting is done
                 print("File Transferred")
                 break
             # we use sendall to assure transimission in busy networks
-            totalRead += bytesRead
-            print("Total Read = " + totalRead + " bytes")
-            print("Percent Complete = " + (totalRead/fileSize) * 100 + " %")
-            s.sendall(bytesRead)
+            totalRead += len(dataRead)
+            print("Total Read = " + str(totalRead) + " bytes")
+            print("Percent Complete = " + str((totalRead/fileSize) * 100) + " %")
+            s.sendall(dataRead)
+
+openConnection()
+sendFile('C:\\Users\\Chris\\Desktop\\bank.txt')
+closeConnection()
