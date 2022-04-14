8731456395c8da5d39ecf718ba36f14cfa84a4f6
Chris <andrei.guevorkian@mail.mcgill.ca>
2022-04-14 18:02:46 -0400

    implemented dynamic page loading of website

--- imagescraper.py
+++ imagescraper.py
@@ -1,4 +1,6 @@
 import requests
+import html
+from requests_html import HTMLSession
 from bs4 import BeautifulSoup  
 import os 
 from urllib.parse import urlparse
@@ -7,24 +9,38 @@ ERROR_BAD_URL = -1
 ERROR_BAD_FILE_PATH = -2
 
 def getdata(url): 
-    r = requests.get(url) 
-    return r.text 
+    # r = requests.get(url)
+    session = HTMLSession()
+    response = session.get(url) 
+    response.html.render(timeout=40)
+    return response.html.find('img')
+    # return response.html.absolute_links
+    # return html.unescape(r.text) 
     
 def getImgs(websiteURL, save_path):
+    print("============================imagescraper htmldata====================")
     try:
         htmldata = getdata(websiteURL)
     except Exception as e:
+        print(e)
         return ERROR_BAD_URL
-
-    soup = BeautifulSoup(htmldata, 'html.parser')
+    print(htmldata)
+    htmldata = [x.html for x in htmldata]
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print(htmldata)
+    soup = BeautifulSoup(' '.join(htmldata), 'html.parser')
     images = soup.find_all('img')
+    print("============================imagescraper images====================")
+    print(images)
+    print("before for loop________________________++++++++++++++++++_________________")
     for index, item in enumerate(images):
         print(item['src'])
         imageurl = item['src']
         path = urlparse(imageurl).path
+        print(path)
         ext = os.path.splitext(path)[1]
         completeFilePath = os.path.join(save_path, f"img{index}{ext}")
-        r = requests.get(item['src'], allow_redirects=True)
+        r = requests.get("https://www.nasa.gov" + item['src'], allow_redirects=True)
         try:
             open(completeFilePath, 'wb').write(r.content)
         except:
