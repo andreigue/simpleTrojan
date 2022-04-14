eb95431687e132afef3e81cf986288acaf97b41b
Chris <andrei.guevorkian@mail.mcgill.ca>
2022-04-14 18:38:40 -0400

    implemented adding base url to relative url paths

--- imagescraper.py
+++ imagescraper.py
@@ -1,3 +1,4 @@
+from tokenize import String
 import requests
 import html
 from requests_html import HTMLSession
@@ -9,38 +10,38 @@ ERROR_BAD_URL = -1
 ERROR_BAD_FILE_PATH = -2
 
 def getdata(url): 
-    # r = requests.get(url)
     session = HTMLSession()
     response = session.get(url) 
     response.html.render(timeout=40)
     return response.html.find('img')
-    # return response.html.absolute_links
-    # return html.unescape(r.text) 
     
+def getBaseURL(websiteURL:str):
+    endPosition = websiteURL[8:].find('/') + 8 # add 8 for first 8 characters skipped ('http://')
+    baseURL = websiteURL[:endPosition]
+    return baseURL
+
+def isRelativeURL(url):
+        return url[0]=='/'
+
 def getImgs(websiteURL, save_path):
-    print("============================imagescraper htmldata====================")
     try:
         htmldata = getdata(websiteURL)
     except Exception as e:
         print(e)
         return ERROR_BAD_URL
-    print(htmldata)
     htmldata = [x.html for x in htmldata]
-    print("++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    print(htmldata)
     soup = BeautifulSoup(' '.join(htmldata), 'html.parser')
     images = soup.find_all('img')
-    print("============================imagescraper images====================")
-    print(images)
-    print("before for loop________________________++++++++++++++++++_________________")
+    baseURL = getBaseURL(websiteURL)
     for index, item in enumerate(images):
-        print(item['src'])
         imageurl = item['src']
         path = urlparse(imageurl).path
-        print(path)
         ext = os.path.splitext(path)[1]
         completeFilePath = os.path.join(save_path, f"img{index}{ext}")
-        r = requests.get("https://www.nasa.gov" + item['src'], allow_redirects=True)
+        if isRelativeURL(item['src']): 
+            r = requests.get(baseURL + item['src'], allow_redirects=True)
+        else:
+            r = requests.get(item['src'], allow_redirects=True)
         try:
             open(completeFilePath, 'wb').write(r.content)
         except:
